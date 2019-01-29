8f8e61bea9b9ea0b5891e4d18f80e32fcea8755d
Lenny <hello@lenny.kr>
2019-01-29 12:52:10 +0100

    Corrected a note & added a header.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+// Set a content-type header for demonstration purposes. 
+header('Content-type: text/plain');
+
 // Set language to Dutch (Flemish)
 putenv('LC_ALL=nl_BE');
 setlocale(LC_ALL, 'nl_BE'); 
@@ -10,7 +13,7 @@ bindtextdomain("myPHPApp", "./locale");
 // Choose domain 
 textdomain("myPHPApp");
 
-// Translation is looking for in ./locale/de_DE/LC_MESSAGES/myPHPApp.mo now
+// Translation is looking for in ./locale/nl_BE/LC_MESSAGES/myPHPApp.mo now
 
 // Print a test message
 echo gettext("Welcome to My PHP Application") . "\n";
